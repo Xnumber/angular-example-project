abe1028f29d51320b9215d06e42c35aa46bb6f81
ClementSyu <clement.syu.csf@gmail.com>
2023-04-18 17:02:00 +0800

    feat: member component
    topic
    - member component start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MemberComponent } from "./member/member.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'member', component: MemberComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

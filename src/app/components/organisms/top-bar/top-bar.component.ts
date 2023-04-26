4f2d89c8e0d91774ce11d113059fba97699a7c93
ClementSyu <clement.syu.csf@gmail.com>
2023-04-26 19:42:41 +0800

    feat: life cycle test
    topic
    - test if state changes in other component would trigger life cycle hook
    - comparing top-bar and peek-a-boo
    
    result
    - life cycle hook would be triggered state changes in other component

--- src/app/components/organisms/top-bar/top-bar.component.ts
+++ src/app/components/organisms/top-bar/top-bar.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./top-bar.component.scss']
 })
 export class TopBarComponent {
+  test: number = 1;
 
+  addTestNumber() {
+    this.test += 1;
+    console.log(this.test)
+  }
 }
